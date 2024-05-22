1620b5d3a8b628d55fcaecdb453b3304a8ece26c
Nat Tan <tresarpnat27@gmail.com>
2024-05-22 15:41:21 +0700

    feat:solv skill checkpoint js part1

--- question-3.js
+++ question-3.js
@@ -1,3 +1,13 @@
 // Question #3
-let userPassword = "";
+let userPassword = "swnalWadqQq";
 // เริ่มเขียนโค้ดตรงนี้
+checkPasswordStrength = (userPassword) => {
+  if (userPassword.length > 10) {
+    return "Strong";
+  } else if (userPassword.length >= 6 && userPassword.length <= 10) {
+    return "Medium";
+  } else {
+    return "Week";
+  }
+};
+console.log(checkPasswordStrength(userPassword));
--- question-4.js
+++ question-4.js
@@ -5,3 +5,14 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+let minQuantity = inventory[0];
+// console.log(minQuantity);
+for (let i = 0; i < inventory.length; i++) {
+  if (inventory[i].quantity < minQuantity.quantity) {
+    minQuantity = inventory[i];
+  }
+}
+// console.log(minQuantity);
+console.log(
+  `สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${minQuantity.name} ซึ่งมี ${minQuantity.quantity}`
+);
